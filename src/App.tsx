cfd24bf97b5bbd274d87d5ddde6f24959f97c8c6
etopritika <dimaprytyka@gmail.com>
2025-04-30 17:07:50 +0300

    Added parallax-provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import { ParallaxProvider } from 'react-scroll-parallax';
 import { FeaturesSection } from './components/FeaturesSection';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 
 function App() {
   return (
-    <>
+    <ParallaxProvider>
       <Header />
       <main>
         <HeroSection />
         <FeaturesSection />
       </main>
-    </>
+    </ParallaxProvider>
   );
 }
 
